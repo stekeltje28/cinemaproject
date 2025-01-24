eb6a0828e100edb9c62b48fefbbaf3fa2ef77a5c
stekeltje28 <thijs.stekeltje@gmail.com>
2025-01-24 12:34:37 +0100

    Update .gitignore en verwijder ongewenste bestanden uit repository

--- pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from pages import views
-
-urlpatterns = [
-    path('', views.index , name='index'),
-    path('account/', include('accounts.urls')),
-    path('', include('films.urls'))
-]
--- reserveringen/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReserveringenConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'reserveringen'
